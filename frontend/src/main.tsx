0482d5ff6b75db89eb2c04b958e85817186fea94
vladjsp <vladjsp@gmail.com>
2024-02-10 17:23:24 +0200

    minor fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,15 @@ import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { store } from './redux/store.ts';
-import './index.css';
 import { Path } from './enums/enums.ts';
-import { SignIn } from './pages';
+import { Home, SignIn, SignUp } from './pages';
+
+import './index.css';
 
 const router = createBrowserRouter([
     {
         path: Path.home,
-        element: <h1>Home</h1>,
+        element: <Home />,
     },
     {
         path: Path.signIn,
@@ -19,7 +20,7 @@ const router = createBrowserRouter([
     },
     {
         path: Path.signUp,
-        element: <h1>Sign In</h1>,
+        element: <SignUp />,
     },
 ]);
 
