c2966097f3349a4577011daa3eac498a0fb38a34
vladjsp <vladjsp@gmail.com>
2024-02-14 21:09:11 +0200

    add sign up page

--- frontend/src/pages/sign-up/SignUp.tsx
+++ frontend/src/pages/sign-up/SignUp.tsx
@@ -1,5 +1,48 @@
+import { Link } from 'react-router-dom';
+import { Button, Input, Layout, PasswordInput } from '../../components';
+import { Card, Form, Row, Space, Typography } from 'antd';
+import { Path } from '../../enums/enums';
+
+const onFinish = (values: FormData) => {
+    console.log('Success:', values);
+};
+
 const SignUp = () => {
-    return <div>SignUp</div>;
+    return (
+        <Layout>
+            <Row align="middle" justify="center">
+                <Card title="Sign In" style={{ width: '30rem' }}>
+                    <Form onFinish={onFinish}>
+                        <Input
+                            name="email"
+                            type="email"
+                            placeholder="Enter your email"
+                            required
+                        />
+                        <PasswordInput
+                            name="password"
+                            placeholder="Enter your password"
+                        />
+                        <PasswordInput
+                            name="confirmPassword"
+                            placeholder="Repeat your password"
+                        />
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Sign In
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                    <Space direction="vertical" size="large">
+                        <Typography.Text>
+                            Already have an account?{' '}
+                            <Link to={Path.signIn}>Sign in</Link>
+                        </Typography.Text>
+                    </Space>
+                </Card>
+            </Row>
+        </Layout>
+    );
 };
 
 export { SignUp };
