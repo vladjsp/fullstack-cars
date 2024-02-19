85644a9445b2bc16e5e9023d76289922c843114c
vladjsp <vladjsp@gmail.com>
2024-02-19 21:29:12 +0200

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,19 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
-import {HttpStatus} from './enums/enums.js'
+import { HttpStatus } from './enums/enums.js';
 
-import {userRouter} from './routes/user.js';
-import {carsRouter} from './routes/cars.js';
+import { userRouter } from './routes/user.js';
+import { carsRouter } from './routes/cars.js';
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -22,24 +24,24 @@ app.use('/user', userRouter);
 app.use('/api/cars', carsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(HttpStatus.NOT_FOUND));
+app.use(function (req, res, next) {
+    next(createError(HttpStatus.NOT_FOUND));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || HttpStatus.INTERNAL_SERVER_ERROR);
-  res.json({
-    error: {
-      message: err.message,
-      stack: req.app.get('env') === 'development' ? err.stack : ''
-    }
-  });
+    // render the error page
+    res.status(err.status || HttpStatus.INTERNAL_SERVER_ERROR);
+    res.json({
+        error: {
+            message: err.message,
+            stack: req.app.get('env') === 'development' ? err.stack : '',
+        },
+    });
 });
 
 export default app;
